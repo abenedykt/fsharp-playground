e8a2d3e956963762decca1b1acfa0ff3d95cc55d
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2014-12-10 19:16:43 +0100

    fizz buzz without tests (in f# :D )

---
+++ src/FizzBuzz/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+
+    let fizzBuzz x = 
+        match x with
+        | x when x % 15 = 0 -> "FizzBuzz"
+        | x when x % 5 = 0 -> "Buzz"
+        | x when x % 3 = 0 -> "Fizz"
+        | _ -> x.ToString()
+    
+    let numbers = [1..100]
+    let result = List.map(fizzBuzz) numbers
+    printfn "%A" result
+    
+    0 // return an integer exit code
