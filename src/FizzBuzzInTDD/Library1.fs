72b1476fbf91252d5a1d9a7d8471539e7dfb0d0e
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2014-12-10 19:26:06 +0100

    added xunit and fluent assertions just to see if it works

---
+++ src/FizzBuzzInTDD/Library1.fs
@@ -0,0 +1,14 @@
+﻿namespace FizzBuzzInTDD
+
+open Xunit
+open FluentAssertions
+
+module EnvironmentTests =
+
+    [<Fact>]
+    let First_unit_test_in_fsharp() =
+        Assert.True(true)
+
+    [<Fact>]
+    let Unit_test_with_fluent_assertions() =
+        true.Should().BeTrue("should be true")
