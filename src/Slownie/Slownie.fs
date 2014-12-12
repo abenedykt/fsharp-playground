3ebc2fb8f01eb20766fb56669af4bc953fbc8ea4
Arkadiusz Benedykt <arkadiusz@benedykt.net>
2014-12-12 07:47:38 +0100

    added skeleton for slownie

---
+++ src/Slownie/Slownie.fs
@@ -0,0 +1,14 @@
+﻿namespace Slownie
+
+module Slownie =
+
+    open FsUnit.Xunit
+    open Xunit
+
+    let Slownie x = "jeden"
+
+    [<Fact>]
+    let ``Zamiana 1 na słownie zwraca 'jeden'``() =
+        1
+        |> Slownie
+        |> should equal "jeden"
